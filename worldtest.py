c61336fe71eb635b34c8937c0cfd5f00412ab260
AwdeDarkar <darkardengeno@gmail.com>
2017-06-03 21:43:03 -0400

    This is interesting

--- worldtest.py
+++ worldtest.py
@@ -4,7 +4,8 @@ import world
 w = world.World(10, 10)
 w.initalizeGrid()
 
-for i in range(0, 10):
-    w.printGrid()
+for i in range(0, 240):
+    #w.printGrid()
     w.runEnergyFlow()
-    input()
+    w.writeImage(i)
+    #input()
